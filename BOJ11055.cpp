014519308b4572750033b3f1d0512009d2b37ca5
서지원 <readyjiwon@gmail.com>
2023-09-30 00:25:25 +0900

    9/28

---
+++ BOJ11055.cpp
@@ -0,0 +1,26 @@
+//2023-09-28.
+//dp[k]=k번째까지 합이 가장 큰 증가하는 부분 수열
+#include "bits/stdc++.h"
+
+using namespace std;
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    vector<int> arr(n, 0);
+    vector<int> dp(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        dp[i] = arr[i];
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] + arr[i]);
+            }
+        }
+    }
+    cout << *max_element(dp.begin(), dp.end());
+}
